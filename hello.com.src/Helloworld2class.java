c929631e8b49022d581b395b6ee7b704779cd8fb
Gaurav <gmshared13@gmail.com>
2020-02-09 13:16:51 +1100

    changed my code

--- hello.com.src/Helloworld2class.java
+++ hello.com.src/Helloworld2class.java
@@ -3,7 +3,12 @@ public class Helloworld2class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.print("This is my Hello world 2 Program");
+    System.out.println("This is my Hello world 2 Program");
+    int i =3;
+    int x = 2;
+    if(i>x){
+    	System.out.println("i is greater than x");
+    }
 	}
 
 }
