afbaef8c9efc4c37830bb0454094f420a0050b8b
Gaurav <gmshared13@gmail.com>
2020-02-09 13:03:42 +1100

    My helloworld 2 project

---
+++ hello.com.src/Helloworld2class.java
@@ -0,0 +1,9 @@
+
+public class Helloworld2class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print("This is my Hello world 2 Program");
+	}
+
+}
